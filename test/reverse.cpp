760147491ac0b8cb66034f8b76f87c7f4e2be2c6
Laurent Forthomme <laurent.forthomme@cern.ch>
2016-05-19 19:38:52 +0200

    Clarified the reverse test

--- test/reverse.cpp
+++ test/reverse.cpp
@@ -4,11 +4,16 @@
 int main(int argc, char* argv[])
 {
   TDCSetup s;
-  s.Dump(2);
+
+  // dump the register content before reverting it
+  std::cout << "--- Before reversing the register:" << std::endl;
+  s.DumpRegister(2);
   std::vector<uint8_t> s_vec = s.GetBytesVector();
   
   TDCSetup s_rev(s_vec, true);
-  s_rev.Dump(2);
+  // dump the register content after reverting it
+  std::cout << "--- After reversing the register:" << std::endl;
+  s_rev.DumpRegister();
 
   return 0;
 }
