2d23c8936ba82bc0d98336029de5d63e78506a56
Laurent Forthomme <lforthomme@protonmail.com>
2016-05-23 14:30:43 +0200

    Update TDCConstants.h
    Changed the registers readout/writing addresses for the I2C driver development

--- include/TDCConstants.h
+++ include/TDCConstants.h
@@ -9,9 +9,9 @@
 #define TDC_BS_BITS_NUM       83
 #define TDC_STATUS_BITS_NUM   62
 
-#define FPGA_SET_SETUP_HDR    0x50
-#define FPGA_GET_SETUP_HDR    0x51
-#define FPGA_GET_CONTROL_HDR  0x52
-#define FPGA_GET_STATUS_HDR   0x54
+#define FPGA_SETUP_ADDR       0x0
+#define FPGA_CONTROL_ADDR     0x1
+#define FPGA_STATUS_ADDR      0x2
+#define FPGA_HPTDCID_ADDR     0x3
 
 #endif
